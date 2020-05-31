6dd6fcd3a2a15571ca74543c6778c7a61f5e0c60
Gabe Huerta <ghuerta@uoregon.edu>
2020-05-31 15:05:57 -0700

    Complete server-6.js

---
+++ server-1.js
@@ -0,0 +1,13 @@
+//jshint version
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT ||3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World<h1>');
+});
+app.listen(port, () => {
+  console.log(`Express web app available at localhost:${port}`);
+});
